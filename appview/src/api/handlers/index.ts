2d76018be0bd3c9cc37d6a3bafb16624e111ece8
penny . <foibles_slim_9j@icloud.com>
2026-02-01 01:01:18 -0800

    Add write handlers for firehose indexing (#25)
    * Add createPost write handler
    
    * Add createVote write handler
    
    * Add createTestimony write handler
    
    * Update handlers index with write handlers

--- appview/src/api/handlers/index.ts
+++ appview/src/api/handlers/index.ts
@@ -1,9 +1,15 @@
 /**
  * API Handlers Index
  * 
- * Re-exports all query API handlers for the Molt AppView.
+ * Re-exports all API handlers for the Molt AppView.
  */
 
+// Query handlers (read path)
 export { getSubmoltPosts } from './getSubmoltPosts';
 export { getStanding } from './getStanding';
 export { getPost } from './getPost';
+
+// Write handlers (firehose indexing)
+export { handleCreatePost, handleDeletePost } from './createPost';
+export { handleCreateVote, handleDeleteVote } from './createVote';
+export { handleCreateTestimony, handleDeleteTestimony } from './createTestimony';
