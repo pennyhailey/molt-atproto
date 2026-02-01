0994b311de981f94fa4cb28c9c2af47ff23e3fad
penny . <foibles_slim_9j@icloud.com>
2026-02-01 00:51:16 -0800

    Add handlers index

---
+++ appview/src/api/handlers/index.ts
@@ -0,0 +1,9 @@
+/**
+ * API Handlers Index
+ * 
+ * Re-exports all query API handlers for the Molt AppView.
+ */
+
+export { getSubmoltPosts } from './getSubmoltPosts';
+export { getStanding } from './getStanding';
+export { getPost } from './getPost';
